750d672b739df2fd0d6cffa401ec0b02b731546f
mateocrespo1021 <matcre01@gmail.com>
2023-10-08 18:19:19 -0500

    iconos

--- src/app/components/habilidades/habilidades.component.ts
+++ src/app/components/habilidades/habilidades.component.ts
@@ -3,10 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-habilidades',
   templateUrl: './habilidades.component.html',
-  styleUrls: ['./habilidades.component.scss']
+  styleUrls: ['./habilidades.component.scss'],
 })
 export class HabilidadesComponent {
-   habilidades=['assets/img/html','assets/img/css','assets/img/scss','assets/img/js','assets/img/angular','assets/img/php','assets/img/github','assets/img/java']
+  habilidades = [
+    'assets/img/html',
+    'assets/img/css',
+    'assets/img/scss',
+    'assets/img/js',
+    'assets/img/angular',
+    'assets/img/php',
+    'assets/img/java',
+    'assets/img/spring',
+  ];
 
-   aprendiendo=['assets/img/nodejs','assets/img/sql']
+  aprendiendo = ['assets/img/nodejs', 'assets/img/sql'];
 }
